d883c488ffe9364238664b3866b4e5d8e9a7b3d9
the3hm <the3hm@gmail.com>
2025-03-25 11:54:39 -0400

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,21 +1,22 @@
 // dataLoader.js
 
-const expansions = ['core', 'dawn', 'warforged']; // Add more sets here
+const expansions = ['core', 'dawn', 'warforged']; // Add more expansion keys here
 
 let allCards = [];
 
 /**
- * Loads all expansion JSONs from the /data folder
+ * Loads all expansion JSONs from the data/expansions folder
  */
 export async function loadAllCards() {
-  const promises = expansions.map(name =>
-    fetch(`/data/${name}.json`).then(res => res.json())
-  );
+  const promises = expansions.map(name => fetch(`/data/expansions/${name}.json`).then(res => res.json()));
   const results = await Promise.all(promises);
   allCards = results.flat();
   return allCards;
 }
 
+/**
+ * Optionally expose cached card data
+ */
 export function getAllCards() {
   return allCards;
 }
